158cd102dc8187ebf9c240aacafbb1710272e9c0
Ryan George <ryan@qdds.nl>
2026-01-27 10:46:15 +0100

    refactor: format DeviceActor type for better readability and add selectors for device snapshots

--- src/devices/actors.ts
+++ src/devices/actors.ts
@@ -3,7 +3,10 @@ import type { AndroidTVDeviceMachineActor } from "./android-tv/machines/device";
 import type { WebOSDeviceMachineActor } from "./lg-webos/machines/device";
 import type { PhilipsDeviceMachineActor } from "./philips-android-tv/machines/device";
 
-export type DeviceActor = WebOSDeviceMachineActor | AndroidTVDeviceMachineActor | PhilipsDeviceMachineActor;
+export type DeviceActor =
+  | WebOSDeviceMachineActor
+  | AndroidTVDeviceMachineActor
+  | PhilipsDeviceMachineActor;
 
 export interface StoredDeviceActor {
   platform: TVPlatform;
